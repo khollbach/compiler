39a5ed4c38b67bba323b759b1a7c895d05ec98f5
George <george.gianacopoulos@mail.utoronto.ca>
2017-02-26 00:10:35 -0500

    more TypeDescriptor progress. equals() is implemented, so are several factory functions

--- Assignments3-5/src/compiler488/symbol/TypeDescriptor.java
+++ Assignments3-5/src/compiler488/symbol/TypeDescriptor.java
@@ -1,45 +1,167 @@
 package compiler488.symbol;
 
+import compiler488.ast.decl.ArrayDeclPart;
+import compiler488.ast.decl.DeclarationPart;
+import compiler488.ast.decl.RoutineDecl;
+import compiler488.ast.decl.ScalarDecl;
+import compiler488.ast.type.BooleanType;
+import compiler488.ast.type.Type;
+
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * Created by gg on 24/02/17.
+ * Class used to keep track of types in the compiler488 SymbolTable
+ *
+ * Created by George Gianacopoulos on 24/02/17.
  */
 abstract class TypeDescriptor {
 
-    private abstract class AtomicTypeDescriptor extends TypeDescriptor {}
+    /*
+    * TODO:
+    *   - factory functions for creating TypeDescriptors from:
+    *       > ScalarDeclaration
+    *       > DeclarationPart + Type
+    *       > RoutineDecl
+    *
+    *     and maybe the following???
+    *       > BoolConstExpn
+    *       > IntConstExpn
+    *       > BinaryExpn (types of required operands? except '=' could have either type...)
+    */
 
-    private class IntegerTypeDescriptor extends AtomicTypeDescriptor {}
+    static ScalarTypeDescriptor create(ScalarDecl decl) {
+        return createScalarTDFromType(decl.getType());
+    }
 
-    private class BooleanTypeDescriptor extends AtomicTypeDescriptor {}
+    static TypeDescriptor create(DeclarationPart declPart, Type type) {
+        ScalarTypeDescriptor scalarTD = createScalarTDFromType(type);
+        if (declPart instanceof ArrayDeclPart) {
+            return new ArrayTypeDescriptor(scalarTD);
+        } else {
+            return scalarTD;
+        }
+    }
 
+    static TypeDescriptor create(RoutineDecl routineDecl) {
+        // create TypeDescriptor for the return type
+        ScalarTypeDescriptor returnType = null;
+        if (routineDecl.getType() != null) {
+            returnType = createScalarTDFromType(routineDecl.getType());
+        }
 
-    private class ArrayTypeDescriptor<E extends AtomicTypeDescriptor> extends TypeDescriptor {}
+        // create TypeDescriptors for the parameters
+        List<ScalarTypeDescriptor> parameterTypes;
+        if (routineDecl.getRoutineBody().getParameters() != null) {
+            parameterTypes =
+                    routineDecl.getRoutineBody().getParameters()
+                    .stream()
+                    .map(TypeDescriptor::create)
+                    .collect(Collectors.toList());
+        } else {
+            parameterTypes = Collections.emptyList();
+        }
 
-    private class FunctionTypeDescriptor extends TypeDescriptor {
+        if (returnType != null) {
+            return new FunctionTypeDescriptor(returnType, parameterTypes);
+        } else {
+            return new ProcedureTypeDescriptor(parameterTypes);
+        }
+    }
+
+    private static ScalarTypeDescriptor createScalarTDFromType(Type type) {
+        if (type instanceof BooleanType) {
+            return new BooleanTypeDescriptor();
+        } else {
+            return new IntegerTypeDescriptor();
+        }
+    }
+
+    private static abstract class ScalarTypeDescriptor extends TypeDescriptor {}
+
+    private static class IntegerTypeDescriptor extends ScalarTypeDescriptor {}
+
+    private static class BooleanTypeDescriptor extends ScalarTypeDescriptor {}
+
+    private static class ArrayTypeDescriptor extends TypeDescriptor {
+
+        /** Represents the type of an array's elements. */
+        final ScalarTypeDescriptor elementType;
+
+        ArrayTypeDescriptor(ScalarTypeDescriptor elementType) {
+            this.elementType = elementType;
+        }
+    }
+
+    private static class FunctionTypeDescriptor extends TypeDescriptor {
 
         /** Represents a function's return typeDescriptor. */
-        final AtomicTypeDescriptor returnType;
+        final ScalarTypeDescriptor returnType;
 
         /** Represents the types of each of a function's parameters. Immutable.*/
-        final List<AtomicTypeDescriptor> parameterTypes;
+        final List<ScalarTypeDescriptor> parameterTypes;
 
-        FunctionTypeDescriptor(AtomicTypeDescriptor returnType, List<AtomicTypeDescriptor> parameterTypes){
+        FunctionTypeDescriptor(ScalarTypeDescriptor returnType,
+                               List<ScalarTypeDescriptor> parameterTypes){
             this.returnType = returnType;
             this.parameterTypes = Collections.unmodifiableList(parameterTypes);
         }
     }
 
-    private class ProcedureTypeDescriptor extends TypeDescriptor {
+    private static class ProcedureTypeDescriptor extends TypeDescriptor {
 
         /**
          * Represents the types of each of a procedure's parameters. Immutable.
          */
-        final List<AtomicTypeDescriptor> parameterTypes;
+        final List<ScalarTypeDescriptor> parameterTypes;
 
-        ProcedureTypeDescriptor(List<AtomicTypeDescriptor> parameterTypes) {
+        ProcedureTypeDescriptor(List<ScalarTypeDescriptor> parameterTypes) {
             this.parameterTypes = Collections.unmodifiableList(parameterTypes);
         }
+    }
+
+    @Override
+    /**
+     * Compares TypeDescriptors for equality.
+     */
+    public boolean equals(Object that) {
+        if (that instanceof TypeDescriptor) {
+            if (this instanceof ScalarTypeDescriptor) {
+                // check if both are of the same scalar type
+                return this.getClass().equals(that.getClass());
+
+            } else if (this instanceof ArrayTypeDescriptor
+                    && that instanceof ArrayTypeDescriptor) {
+                // check if both array type descriptors have the same
+                // element type
+                ArrayTypeDescriptor atThis = (ArrayTypeDescriptor) this;
+                ArrayTypeDescriptor atThat = (ArrayTypeDescriptor) that;
+                return atThis.elementType.equals(atThat.elementType);
+
+            } else if (this instanceof FunctionTypeDescriptor
+                    && that instanceof FunctionTypeDescriptor) {
+                // check if both function type descriptors have the same
+                // return type and parameter types
+                FunctionTypeDescriptor ftThis = (FunctionTypeDescriptor) this;
+                FunctionTypeDescriptor ftThat = (FunctionTypeDescriptor) that;
+                boolean sameReturnTypes =
+                        ftThis.returnType.equals(ftThat.returnType);
+                boolean sameParameterTypes =
+                        ftThis.parameterTypes.equals(ftThat.parameterTypes);
+
+                return sameParameterTypes && sameReturnTypes;
+            } else if (this instanceof ProcedureTypeDescriptor
+                    && that instanceof ProcedureTypeDescriptor) {
+                // check if both procedure type descriptors have the same signature
+                ProcedureTypeDescriptor ptThis = (ProcedureTypeDescriptor) this;
+                ProcedureTypeDescriptor ptThat = (ProcedureTypeDescriptor) that;
+
+                return ptThis.parameterTypes.equals(ptThat.parameterTypes);
+            }
+        }
+
+        return false;
     }
 }
